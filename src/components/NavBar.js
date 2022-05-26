1c2978d21a93e6e69592190038776d0a23edd85e
zeeshan0007 <zeeshanmehar305@gmail.com>
2022-05-26 15:57:26 +0500

    trying to pull

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,7 @@ class Navbar extends Component {
 	render = () => (
 		<div className="col-12">
 			<h2 className="bg-primary text-white text-center p2">
-				{this.props.name}'s To do list
+				{this.props.name}'s To do listsssssssssssssssssssssssssssssssssssssss
 			</h2>
 		</div>
 	);
